b9d05414f48f0cda58e3b0021e7b68cf32fcd15e
J-Street015 <johan.streetman85@gmail.com>
2025-12-04 09:14:29 +0100

    use enumerate and dict

--- test_field.py
+++ test_field.py
@@ -1,16 +1,27 @@
-list = [2712233521522212239633525221424223292522332923342263323223226223332531222232333293222213262324223122,
-6443848847769438847664244676354493684774344514352544147447353899987328644494647946726462934543554474,
-3332433333233333633323332331333325311333423333343552333343435324242433322332243454433334433332333241,
-6534444453544144554565453444455534444525545454555544534444654445544543445544455555444224441534555454,]
+
 
 
 
 # turn each large number in a list itself and then do the pop method
 
 
-list = [1, 3, 9, 5, 6 ]
+num = 2712233521522212239633525221424223292522332923342263323223226223332531222232333293222213262324223122
 
-while len(list) >= 2:
+# make list from the numbers
+num_list = list(str(num))
+# create a dictionary from the list of numbers with the number strings as keys.
+# this removes the duplicated digits, but also keeps the order of numbers.
+num_dict  = dict.fromkeys(list(str(num)))
 
+num_list_updated = []
+for key, value in num_dict.items():
+    num_list_updated.append(int(key))
 
-print(max(list))
+print(num_list_updated)
+
+# find the two highest digits in the list
+
+for i, num in enumerate(num_list_updated):
+    num1 = (num, i)
+    print(num1)
+
