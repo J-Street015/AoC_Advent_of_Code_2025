79bd87b8603f52a26161390d515cf3c20b0346e4
J-Street015 <johan.streetman85@gmail.com>
2025-12-15 15:57:45 +0100

    challgen day6 pt fix

--- d7_pt1.py
+++ d7_pt1.py
@@ -4,18 +4,18 @@ with open("d7_test") as f:
         data.append(line.rstrip("\n"))
 
 
-# for i in range(len(data)):
-#     if all(problem[i] == '.' for problem in data):
-#         print(f"here is a dot: {i}")
-
-
-# print(data)
-
-
 for i in range(len(data[0])):
     if all(position[i] == "." for position in data):
 
        print(" ".join(position[i] for position in data)) # gives the columns where there are no carets
 
 
+
+test_dat = ["a", "b", "c"]
+
+for line in data:
+    for i in range(len(line)):
+        print(line[i])
+        if line[i] == "^":
+            print(line.index(line[i -1]), line.index(line[i +1]))
 
