1701b2b202e34a700374798a20e360d0f3f5e70c
J-Street015 <johan.streetman85@gmail.com>
2025-12-17 18:25:00 +0100

    day6 p2 fix, d7_p1 start

--- d7_pt1.py
+++ d7_pt1.py
@@ -3,19 +3,30 @@ with open("d7_test") as f:
     for line in f.readlines():
         data.append(line.rstrip("\n"))
 
+#
+# for i in range(len(data[0])):
+#     if all(position[i] == "." for position in data):
+#
+#        print(" ".join(position[i] for position in data)) # gives the columns where there are no carets
+#
 
-for i in range(len(data[0])):
-    if all(position[i] == "." for position in data):
+start_pos = int()
+# find the starting position
+for line in data:
+    if "S" in line:
+        start_pos = line.index("S")
 
-       print(" ".join(position[i] for position in data)) # gives the columns where there are no carets
-
-
-
-test_dat = ["a", "b", "c"]
+print(start_pos)
 
 for line in data:
-    for i in range(len(line)):
-        print(line[i])
-        if line[i] == "^":
-            print(line.index(line[i -1]), line.index(line[i +1]))
+    if "^" in line[start_pos]:
+        print(f"first split at: {data.index(line)}")
+        # try to get the upstream and downstream positions of where the split happens.
+        # temp_pos = [position for position in line if "^" in line[start_pos]]
+        # print(temp_pos)
+        print(start_pos -1, start_pos +1)
+
+
+
+
 
