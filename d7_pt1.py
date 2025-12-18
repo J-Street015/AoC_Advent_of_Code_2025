2dee38119d450d0f9a8cd2b58b57e3c1a4ef1892
J-Street015 <johan.streetman85@gmail.com>
2025-12-18 19:54:31 +0100

    day7 pt1 fixed

--- d7_pt1.py
+++ d7_pt1.py
@@ -1,37 +1,25 @@
+# read in the data
 data = []
 with open("d7_input") as f:
     for line in f.readlines():
         data.append(line.rstrip("\n"))
 
 
+# define start position
+beam_pos = set([i for i, x in enumerate(data[0]) if x == "S"]) # set the start position as the first split position
 
-# get the start position:
-start_pos = [data[0].index("S")]
-
-split_pos = start_pos # set the start position as the first split position
-test = []
 split_count = 0
 for line in data:
-    indices = [i for i, x in enumerate(line) if x == "^"] # records all instances of caret in line and their indices.
-    print(indices)
-    temp = []
+    idxs = [i for i, x in enumerate(line) if x == "^"] # records all instances of caret in line and their indices.
+    beam_pos = list(set(beam_pos)) # convert beam position to set to avoid duplicates in case the beam splits into same postion
 
-    for i in indices:
-        if i in split_pos:
-            split_count += 1
-            lower = i -1
-            higher = i +1
-            split_pos.append(lower)
-            split_pos.append(higher)
-            temp.append(lower)
-            temp.append(higher)
-    if len(temp) > 0:
-        test.append(set(temp))
-            # try create a set of the numbers
-
-# print(split_pos)
-print(test)
+    # go through all positions of the splitter and check if splitter is at the beam position
+    for i in idxs:
+        if i in beam_pos:
+            split_count += 1 # update split if a splitter is in beam position
+            left_idx = i -1 #get left beam
+            right_idx = i +1 #get right beam
+            beam_pos.append(left_idx)
+            beam_pos.append(right_idx)
+            beam_pos.pop(beam_pos.index(i))
 print(split_count)
-
-
-
