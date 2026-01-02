5f92fe28a7dd85e1c1a2a296a636fe3427e44598
J-Street015 <johan.streetman85@gmail.com>
2026-01-02 20:05:15 +0100

    day8 pt 2 finish

---
+++ d8_pt2.py
@@ -0,0 +1,76 @@
+import math
+from typing import List
+
+import pandas as pd
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+import heapq # For efficient top-N selection
+
+
+dat = pd.read_csv('d8_full', sep=",", header=None, index_col=False)
+dat = dat.rename(columns={0:"X", 1:"Y", 2:"Z"})
+
+# Array of points (each row is a 3D point)
+points = np.array(dat)
+# print(points_array_dat)
+
+# print(points)
+
+distances = []
+
+for i in range(len(points)):
+    # print("index:", i, points[i])
+    for j in range(i+1, len(points)):
+        p1, p2 = points[i], points[j]
+        d = math.dist(p1,p2)
+        distances.append(((i,j), d))
+
+# print(distances) # distances before sorting
+# print('#'*40)
+sorted_dist = sorted(distances, key=lambda x: x[1]) # sorts the list of tuples by the distance but keeps the
+# of the pairs
+
+# print(distances) # distances after sorting
+
+def init_circuits(num_points: int) -> List[set]:
+    return [{i} for i in range(num_points)]
+
+circuits = init_circuits(len(points))
+print(circuits)
+
+def connect(circuits: List[set], p1: int, p2: int) -> None:
+    set1 = set2 = None
+
+    for s in circuits:
+        if p1 in s:
+            set1 = s
+        if p2 in s:
+            set2 = s
+
+    if set1 is not set2:
+        set1.update(set2)
+        circuits.remove(set2)
+
+for i in range(1000):
+    ((p1, p2), _) = sorted_dist[i]
+    connect(circuits, p1, p2)
+
+circuits = sorted(circuits, key=lambda x: len(x), reverse=True)
+
+# print(circuits)
+
+
+def part_two(points: List[str]) -> int:
+    distances = sorted_dist
+    circuits = init_circuits(len(points))
+
+    i = 0
+    p1 = p2 = -1
+    while len(circuits) != 1:
+        ((p1, p2), _) = distances[i]
+        connect(circuits, p1, p2)
+        i += 1
+
+    return points[p1][0] * points[p2][0]
+
+print(part_two(points))
