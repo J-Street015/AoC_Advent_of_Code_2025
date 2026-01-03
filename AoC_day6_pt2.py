8fbfd706d5d3e8ab5e126ee56941d6877c9e9816
J-Street015 <johan.streetman85@gmail.com>
2026-01-03 18:14:50 +0100

    AoC day6 part2 clean up

--- AoC_day6_pt2.py
+++ AoC_day6_pt2.py
@@ -1,26 +1,19 @@
-# Code is maybe not good looking but it works.
+# AoC day6 part 2. Code is maybe not good-looking, but it works.
+from math import prod
 
 number_map = []
-with open("d6_test") as f:
+with open("d6_input") as f:
     for line in f.readlines():
         number_map.append(line.rstrip("\n"))
-# print(number_map)
 
-from math import prod
-from typing import List
 
-def part_two(data: List[str]) -> int:
-    numbers = data[:-1] # Keep as strings
-    print(numbers)
-    print(data)
-    operators = data[-1].split()   # Last line contains operations
-
+def calculation(data):
+    numbers = data[:-1]  # Keep as strings
+    operators = data[-1].split()  # Last line contains operations
     total = 0
     digits = []
-
     # while numbers are not at position -1, they get added to numbers, if loop reaches pos -1 instead
     # of adding the code sums or multiplies the numbers collected so far, and resets the number list
-
     # # Iterate column-wise from right to left
     for i in range(len(numbers[0]) - 1, -2, -1):
         # If the column is empty (all spaces) or end of row
@@ -40,4 +33,5 @@ def part_two(data: List[str]) -> int:
 
     return total
 
-print(part_two(number_map))
+
+print(calculation(number_map))
