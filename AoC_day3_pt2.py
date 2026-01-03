c41665913115c2dd766546fb52aa7f5d7c831756
J-Street015 <johan.streetman85@gmail.com>
2026-01-03 17:01:39 +0100

    AoC day3 pt2 clean up

--- AoC_day3_pt2.py
+++ AoC_day3_pt2.py
@@ -5,17 +5,16 @@ with open("input_day3") as f:
         num_list.append(int(line.rstrip("\n")))
 
 
+# function found at https://www.geeksforgeeks.org/dsa/largest-number-possible-after-removal-of-k-digits/
 def maxnumber(n, k):
     # Function to return the
     # largest number possible
-
     for i in range(0, k):
         # Generate the largest number
         # after removal of the least K digits
         # one by one
         ans = 0
         i = 1
-
         while n // i > 0:
             # Remove the least digit
             # after every iteration
@@ -31,10 +30,14 @@ def maxnumber(n, k):
 
         # Return the remaining number
     # after K removals
-    return ans;
+    return ans
 
 
-k = 88
+# get k for the function.
+number_len = len(str(num_list[0]))
+final_number_length = 12
+
+k = number_len - final_number_length
 updated_num_list = []
 for n in num_list:
     updated_num_list.append(maxnumber(n, k))
