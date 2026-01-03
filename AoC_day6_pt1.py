da184543b69b0909e52f4900606ae7260b233c05
J-Street015 <johan.streetman85@gmail.com>
2026-01-03 18:07:45 +0100

    AoC day6 part1 clean up

--- AoC_day6_pt1.py
+++ AoC_day6_pt1.py
@@ -1,15 +1,13 @@
-# Code is maybe not good looking but it works.
+# AoC day6 pt1. Code does not look good but works.
 
 import pandas as pd
 
 dat = pd.read_csv("d6_input", sep='\s+',header=None)
-
 dat = pd.DataFrame(dat)
 
 num = 0
 num_sum = []
 for i in dat:
-    # print(dat[i]) # prints columns 1 by 1
     test = []
     for i in dat[i]:
         test.append(i)
@@ -21,11 +19,12 @@ for i in dat:
             num = 0
 
         elif "*" in test:
-            test.pop(len(test) -1)
-            num = num +1
+            test.pop(len(test) - 1)
+            num = num + 1
             for i in test:
                 num *= int(i)
             num_sum.append(num)
             num = 0
+
 print(sum(num_sum))
 
